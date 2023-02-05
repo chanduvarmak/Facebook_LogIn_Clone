cb2c74bb21ef52db94c601e45c40d1a9db8e5d3e
chandu varma.k <119986528+chanduvarmak@users.noreply.github.com>
2023-02-05 21:03:42 +0530

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,16 @@
+function validate()
+{
+    var username=document.getElementById("username").value
+    var password=document.getElementById("password").value
+    if(username=="admin"&&password=="user")
+    {
+        alert("login succesfull")
+        return false;
+    }
+    else
+    {
+        alert("login failed")
+    }
+
+
+}
